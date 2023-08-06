86886bcd6ef5fa2b7377413ac101b17d88e9a53c
Hannah Morrison <hannah@landhmorrison.scot>
2023-08-06 12:04:15 +0100

    Add kinetic mouse wheel

--- config.h
+++ config.h
@@ -17,16 +17,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// Set the mouse settings to a comfortable speed/accuracy trade-off,
-// assuming a screen refresh rate of 60 Htz or higher
-// The default is 50. This makes the mouse ~3 times faster and more accurate
+// Enable kinetic mouse
+#define MK_KINETIC_SPEED
+
+// Set the mouse settings to a comfortable speed/accuracy trade-off, assuming a
+// screen refresh rate of 60 Htz or higher The default is 50. This makes the
+// mouse ~3 times faster and more accurate
 #define MOUSEKEY_INTERVAL 16
 
-// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
-// give it more time to accelerate to max speed to retain precise control over short distances.
+// The default is 20. Since we made the mouse about 3 times faster with the
+// previous setting, give it more time to accelerate to max speed to retain
+// precise control over short distances.
 #define MOUSEKEY_TIME_TO_MAX 40
 
-// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
+// The default is 300. Let's try and make this as low as possible while keeping
+// the cursor responsive
 #define MOUSEKEY_DELAY 100
 
 // It makes sense to use the same delay for the mouseweel
@@ -38,6 +43,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // The default is 40
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
+// Vaguely tuned settings for kinetic mouse wheel
+#define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 12
+#define MOUSEKEY_WHEEL_BASE_MOVEMENTS 32
+#define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 32
+#define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 12
+
 // Default tapping term
 #define TAPPING_TERM 190
 #define PERMISSIVE_HOLD
@@ -48,20 +59,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Enable key repeat - disabled for now
 // #define QUICK_TAP_TERM 100
-
-// Underglow configuration
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#endif
